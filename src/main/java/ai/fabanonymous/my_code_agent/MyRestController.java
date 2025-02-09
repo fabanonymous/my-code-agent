1d302ddc0e2935266d8eeded7e79cb1932ef0fcf
fabanonymous <fabanonymous@yahoo.fr>
2025-02-09 09:59:44 +0100

    ChatLikePirate added for tests

--- src/main/java/ai/fabanonymous/my_code_agent/MyRestController.java
+++ src/main/java/ai/fabanonymous/my_code_agent/MyRestController.java
@@ -17,17 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyRestController {
 
     private final ChatClient chatClient;
+    private final ChatClient chatClientForChatLikePirate;
 
     public MyRestController(ChatClient.Builder builder, VectorStore vectorStore) {
         this.chatClient = builder
                 .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
+        this.chatClientForChatLikePirate = builder
+                .defaultSystem("You are a helpful assistant that replies to user messages as if you were the Blackbeard Pirate.")
+                .build();
     }
 
-    // http :8080/test message=="How did the Federal Reserve's recent interest rate cut impact various asset classes according to the analysis"
-    @GetMapping("/test")
-    public String test(@RequestParam(value = "message", defaultValue = "How did the Federal Reserve's recent interest rate cut impact various asset classes according to the analysis") String message) {
+    // http :8080/test/rag message=="How did the Federal Reserve's recent interest rate cut impact various asset classes according to the analysis"
+    @GetMapping("/test/rag")
+    public String testRag(@RequestParam(value = "message", defaultValue = "How did the Federal Reserve's recent interest rate cut impact various asset classes according to the analysis") String message) {
         return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+    // http :8080/test/chat message=="Say Hello"
+    @GetMapping("/test/chat")
+    public String testChatLikePirate(@RequestParam(value = "message", defaultValue = "Say Hello") String message) {
+        return chatClientForChatLikePirate.prompt()
                 .user(message)
                 .call()
                 .content();
